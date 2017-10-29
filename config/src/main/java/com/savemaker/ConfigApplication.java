5708a8ec560b69867f3ebe87747254f22f0baa82
lukasz.rys <lukasz.rys@me.com>
2017-10-29 21:26:25 +0100

    Added Config Server with git configuration (without eureka yet)

---
+++ config/src/main/java/com/savemaker/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.savemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
---
+++ config/src/test/java/com/savemaker/ConfigApplicationTest.java
@@ -0,0 +1,29 @@
+package com.savemaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConfigApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ConfigApplicationTest {
+
+    @Value("${local.server.port}")
+    private int port = 0;
+
+    @Test
+    public void configurationSuccess() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:" + port + "/app/cloud", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+}
